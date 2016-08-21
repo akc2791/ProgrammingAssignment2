f0f8decca8c16e13bf87768f36034cae4e28cbf8
Sridhar2791 <c.anandkrishnan@gmail.com>
2016-08-20 17:30:49 -0700

    Revert "Cachematrix file"
    This reverts commit 50eee66c091b2888c8911cc0215f5dfca4041655.

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,15 @@
-## The two functions here peform 2 jobs - 1st function caches the inverse of a matrix 
-## and the second one checks the cache and outputs the result. Detailed explanations follow
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function takes the inverse of a square matrix and stores the result in a cache variable.
-## This cache variable are stored in a environment which is diff from that of the function.
+## Write a short comment describing this function
 
-makeCacheMatrix <- function(x=matrix()) {    #making the function
-    m <- NULL                       
-    set <- function(y) {            #making the set function
-        x <<- y
-        m <<- NULL
-    }
-    get <- function() x             #
-    setinverse <- function(solve) m <<- solve
-    getinverse <- function() m
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
-## This function checks the cached variables and if the answer is available in the variable in it then it just takes it from there.
-## else it calculates the inverse of the variable
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-    m <- x$getinverse()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    m <- solve(data, ...)
-    x$setinverse(m)
-    m
+        ## Return a matrix that is the inverse of 'x'
 }
